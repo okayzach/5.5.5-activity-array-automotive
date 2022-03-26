5f247994da4e05cb972ac79b35abdebfaca02e01
okayzach <znortyy@gmail.com>
2022-03-25 22:23:01 -0400

    finished instructions

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,33 @@ const VehicleModule = require("./vehicle")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    loadPassenger(num){
+        if (num + this.passenger < this.maximumPassengers){
+            availableRoom = true
+            return this.passenger
+        }
+    }
+    start(){
+        if(this.fuel > 0){
+            return start = true
+        } else
+    }
+    scheduleService(){
+        if(this.mileage > 30000){
+            return this.scheduleService = true
+        }
+    }
+
+
+}
